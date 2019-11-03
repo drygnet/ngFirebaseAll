862a1c30256cf3863b07feed6fef1d3169a5b2f5
Johan Blomgren <johan@dryg.net>
2019-11-03 18:02:57 +0100

    storage rules

--- src/app/files/files.component.ts
+++ src/app/files/files.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { finalize } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 
 @Component({
@@ -12,14 +13,16 @@ import { Observable } from 'rxjs';
 export class FilesComponent implements OnInit {
   uploadPercent: Observable<number>;
   downloadURL: Observable<string>;
-  constructor(private storage: AngularFireStorage) { }
+  constructor(private storage: AngularFireStorage, private afAuth: AngularFireAuth) {
+  }
 
   ngOnInit() {
   }
 
   uploadFile(event) {
     const file = event.target.files[0];
-    const filePath = 'name-your-file-path-here';
+    console.log(file);
+    const filePath = `users/${this.afAuth.auth.currentUser.uid}wrong/${new Date().getTime()}_${file.name}`;
     const fileRef = this.storage.ref(filePath);
     const task = this.storage.upload(filePath, file);
 
