3bca8d77676b3b3a2d4e015079ebe0e9d84e75b7
Johan Blomgren <johan@dryg.net>
2019-11-03 21:21:43 +0100

    file object fail [BROKEN]

--- src/app/files/files.component.ts
+++ src/app/files/files.component.ts
@@ -3,6 +3,7 @@ import { AngularFireStorage } from '@angular/fire/storage';
 import { finalize } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database'; // realtime database
 
 
 @Component({
@@ -13,16 +14,22 @@ import { AngularFireAuth } from '@angular/fire/auth';
 export class FilesComponent implements OnInit {
   uploadPercent: Observable<number>;
   downloadURL: Observable<string>;
-  constructor(private storage: AngularFireStorage, private afAuth: AngularFireAuth) {
+  fileList: Observable<any>;
+  basePath = `users/${this.afAuth.auth.currentUser.uid}/`;
+  constructor(
+    private storage: AngularFireStorage,
+    private afAuth: AngularFireAuth,
+    private db: AngularFireDatabase) {
   }
 
   ngOnInit() {
+    console.log('base', this.basePath);
+
   }
 
   uploadFile(event) {
     const file = event.target.files[0];
-    console.log(file);
-    const filePath = `users/${this.afAuth.auth.currentUser.uid}wrong/${new Date().getTime()}_${file.name}`;
+    const filePath = `users/${this.afAuth.auth.currentUser.uid}/${new Date().getTime()}_${file.name}`;
     const fileRef = this.storage.ref(filePath);
     const task = this.storage.upload(filePath, file);
 
@@ -30,9 +37,21 @@ export class FilesComponent implements OnInit {
     this.uploadPercent = task.percentageChanges();
     // get notified when the download URL is available
     task.snapshotChanges().pipe(
-      finalize(() => this.downloadURL = fileRef.getDownloadURL())
+      finalize(() => {
+        fileRef.getDownloadURL().subscribe(downloadURL => {
+          console.log('File available at', downloadURL);
+          file.DownloadUrl = downloadURL;
+          file.name = file.name;
+          file.size = file.size;
+          this.saveFileData(file);
+        });
+      })
     )
       .subscribe();
+  }
+
+  private saveFileData(fileData) {
+    this.db.list(this.basePath).push(fileData);
   }
 
 }
